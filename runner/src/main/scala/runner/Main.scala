be8de687c9220cee41b4b35ea7f89e345b036c44
leonardo <naddeileonardo@gmail.com>
2026-09-20 23:13:38 +0200

    Relazione

--- runner/src/main/scala/runner/Main.scala
+++ runner/src/main/scala/runner/Main.scala
@@ -1,8 +1,6 @@
 package runner
 
-import fuse.{FusedStream, Collector}
-import FusedStream.*
-
+import fuse.FusedStream.*
 
 @main def hello(): Unit = {
   val i = 10
@@ -10,19 +8,10 @@ import FusedStream.*
   val numbers: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   val result = FusedStream
     .from(numbers)
-    .filter(_ % 2 == 0)
-    .filter(_ > 0)
-    .filter(_ > 1)
-    .filter(_ > 2)
+    .filter(i => i % 2 == 0)
     .skip(1)
-    .map(_ * 10)
-    .map(_ * 20)
-    .map(_ * 30)
-    .flatMap(j => {
-      FusedStream.of(j)
-      // FusedStream.from((i * 10 to i * 10 + 9).toList)
-    })
-    .collect(Collector.findFirst[Int])
+    .map(i => i * 10)
+    .collect(summing)
 
   println(result)
 
